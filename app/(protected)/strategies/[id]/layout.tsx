e38c9dfdf95fcce90b85f3eddd5086cc8314210d
unknown <alonsog.jaime@gmail.com>
2025-06-03 01:38:28 +0200

    feat: <tfc>

--- app/(protected)/strategies/[id]/layout.tsx
+++ app/(protected)/strategies/[id]/layout.tsx
@@ -3,7 +3,7 @@ import GoBack from "@/components/dashboard/ui/go-back";
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col gap-6">
-            <GoBack route="/strategies" routeName="strategies" />
+            <GoBack routeName="strategies" />
             {children}
         </div>
     );
